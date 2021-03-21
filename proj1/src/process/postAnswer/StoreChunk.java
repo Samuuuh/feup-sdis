302931b08a184c62252ddd1c1ef226f5830d67b1
Diogo Samuel Fernandes <up201806250@fe.up.pt>
2021-03-21 15:43:24 +0000

    End on last chunk

--- proj1/src/process/postAnswer/StoreChunk.java
+++ proj1/src/process/postAnswer/StoreChunk.java
@@ -3,6 +3,7 @@ package process.postAnswer;
 import channel.MessageParser;
 import file.FileHandler;
 import main.Peer;
+import state.FileState;
 
 import java.io.IOException;
 
@@ -17,9 +18,12 @@ public class StoreChunk extends Thread {
     public void run() {
         FileHandler.saveFileChunks(messageParsed, "peers/peer_" + Peer.peer_no + "/restore/");
 
-        if (messageParsed.getChunkNo().equals("1")) {
+        FileState fileState = Peer.peer_state.fileHash.get(messageParsed.getFileId());
+        int chunkNo = fileState.getChunkStateHash().size();
+
+        if (Integer.parseInt(messageParsed.getChunkNo()) == (chunkNo-1)) {
             try {
-                FileHandler.saveFile(messageParsed.getFileId(), "peers/peer_" + Peer.peer_no + "/restore/", 1);
+                FileHandler.saveFile(messageParsed.getFileId(), "peers/peer_" + Peer.peer_no + "/restore/", chunkNo-1);
             } catch (IOException e) {
                 e.printStackTrace();
             }
