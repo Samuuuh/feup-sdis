3c7157547b510e7fee69bd0cdd2917cad2b8f5fa
Diogo Samuel Fernandes <up201806250@fe.up.pt>
2021-04-08 11:56:10 +0000

    Add bash files

--- proj1/src/bash.sh
+++ proj1/src/bash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function p() {
-        java main.Peer 2.0 $1 230.0.0.1 1889 230.0.0.2 1888 230.0.0.3 1887
+        java -cp build main.Peer 1 $1 230.0.0.1 1889 230.0.0.2 1888 230.0.0.3 1887
 }
 
 function c() {
-        java main.Client $1 $2 $3 $4
+        java -cp build main.Client $1 $2 $3 $4
 }
