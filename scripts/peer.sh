103ea6fd7ee072a8568a10bee409c5edba85dda0
Diogo Samuel Fernandes <up201806250@fe.up.pt>
2021-06-02 08:52:06 -0400

    Scripts

---
+++ scripts/peer.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash
+
+# Check number input arguments
+argc=$#
+
+if (( argc == 2 )) 
+then
+    # Assign input arguments to nicely named variables
+    ip=$1
+    port=$2
+    
+    # Execute the program
+    sudo java -Djavax.net.ssl.keyStore=../keystore -Djavax.net.ssl.keyStorePassword=123456 -Djavax.net.ssl.trustStore=../truststore -Djavax.net.ssl.trustStorePassword=123456 network/Main ${ip} ${port}
+fi
+
+if (( argc == 4 )) 
+then
+    # Assign input arguments to nicely named variables
+    ip=$1
+    port=$2
+    chord_ip=$3
+    chord_port=$4
+
+    # Execute the program
+    sudo java -Djavax.net.ssl.keyStore=../keystore -Djavax.net.ssl.keyStorePassword=123456 -Djavax.net.ssl.trustStore=../truststore -Djavax.net.ssl.trustStorePassword=123456 network/Main ${ip} ${port} ${chord_ip} ${chord_port}
+fi
